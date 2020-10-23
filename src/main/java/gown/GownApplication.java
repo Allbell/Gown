82720ad8a5580aa861f41e300b057ef59199b63f
Bell <neeq@ro.ru>
2020-10-23 10:05:10 +0300

    [add] gown project

---
+++ src/main/java/gown/GownApplication.java
@@ -0,0 +1,11 @@
+package gown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GownApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GownApplication.class, args);
+    }
+}
